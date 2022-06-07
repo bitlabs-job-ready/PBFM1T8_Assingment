c2f487ef995d846805a2b956467b585d9dca99be
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-06-07 12:07:41 +0530

    Add files via upload

---
+++ PBFM1T8_Assingment3.py
@@ -0,0 +1,15 @@
+"""
+write a program to count the occurence of a specific character in a string
+
+Example String:
+To count the occurrence of a spicific character in a string
+
+Input:
+
+which character's occurence do you want to count? : c
+Enter string : character
+
+Output:
+The character c has appeared 2 times.
+
+"""
